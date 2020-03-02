71e42279c32ee5f8f56f9f878462076857a354e4
Andrew Galyan-Mann <andrew.galyanmann@icloud.com>
2020-03-01 23:27:31 -0500

    Add dynamic search

--- js/script.js
+++ js/script.js
@@ -2,6 +2,14 @@
  * Create and append search input to DOM
  */
 const searchDiv = document.querySelector(".search-container");
+const userUrl = "https://randomuser.me/api/?results=12&nat=us";
+const galleryDiv = document.querySelector("#gallery");
+const body = document.querySelector("body");
+let users = [];
+const searchError = document.createElement("h1");
+searchError.style.display = "none";
+galleryDiv.append(searchError);
+searchError.innerText = "Sorry, there are no employees that match your search.";
 
 searchDiv.innerHTML = `
     <form action="#" method="get">
@@ -10,21 +18,32 @@ searchDiv.innerHTML = `
     </form>
 `;
 
-document.querySelector('#search-input').focus();
+searchDiv.addEventListener("keyup", () => {
+  searchField(
+    document.querySelector("#search-input"),
+    document.querySelectorAll(".card-name")
+  );
+});
 
-searchDiv.addEventListener('keyup', event => {
-  console.log(event.key);
-})
+function searchField(input, searchList) {
+  let results = [];
+  searchList.forEach(user => {
+    user.parentNode.parentNode.style.display = "none";
+    if (user.textContent.toLowerCase().includes(input.value.toLowerCase())) {
+      user.parentNode.parentNode.style.display = "";
+      results.push(user);
+    }
+  });
+  if (input !== "" && results.length === 0) {
+    searchError.style.display = "";
+  } else {
+    searchError.style.display = "none";
+  }
+}
 
 /**
  * MAIN GALLERY
  */
-
-const userUrl =
-  "https://randomuser.me/api/?results=12&nat=us";
-const galleryDiv = document.querySelector("#gallery");
-const body = document.querySelector("body");
-let users = [];
 
 getUser(userUrl);
 
@@ -45,7 +64,7 @@ function getUser(api) {
  * Maps over users array to generate HTML
  */
 function generateHTML() {
-  users.map(async (user) => {
+  users.map(async user => {
     const cardDiv = document.createElement("div");
     const modalDiv = document.createElement("div");
     const script = document.querySelector("script");
@@ -69,16 +88,29 @@ function generateHTML() {
 
     modalDiv.innerHTML = `
       <div class="modal">
-        <button type="button" id="${user.name.first}-${user.name.last}-modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <button type="button" id="${user.name.first}-${
+      user.name.last
+    }-modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
-          <img class="modal-img" src="${user.picture.large}" alt="profile picture">
-          <h3 id="${user.name.first}-${user.name.last}-modal" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
+          <img class="modal-img" src="${
+            user.picture.large
+          }" alt="profile picture">
+          <h3 id="${user.name.first}-${
+      user.name.last
+    }-modal" class="modal-name cap">${user.name.first} ${user.name.last}</h3>
           <p class="modal-text">${user.email}</p>
           <p class="modal-text cap">${user.location.city}</p>
           <hr>
           <p class="modal-text>${user.cell}</p>
-          <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.city}, ${user.location.state} ${user.location.postcode}</p>
-          <p class="modal-text">Birthday: ${user.dob.date.substring(5, 7)}-${user.dob.date.substring(8, 10)}-${user.dob.date.substring(0, 4)}
+          <p class="modal-text">${user.location.street.number} ${
+      user.location.street.name
+    }, ${user.location.city}, ${user.location.state} ${
+      user.location.postcode
+    }</p>
+          <p class="modal-text">Birthday: ${user.dob.date.substring(
+            5,
+            7
+          )}-${user.dob.date.substring(8, 10)}-${user.dob.date.substring(0, 4)}
           </p>
         </div>
       </div> 
